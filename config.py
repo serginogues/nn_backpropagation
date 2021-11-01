3c6860ae4e15f470919db805ebe71e13b7ce3fa6
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-11-01 19:18:59 +0100

    print

--- config.py
+++ config.py
@@ -1,20 +1,16 @@
-import numpy as np
-
 # Do not change
 INPUT_DIM = 8
 HIDDEN_DIM = 3
 OUTPUT_DIM = 8
+EPOCHS = 50000
 
 # Hyperparams to tune
 LR = 0.01  # learning rate
 WD = 0.001  # weight decay
-EPOCHS = 40000
-
-
+WEIGHT_INIT = 0
 """
-Weight initialization approach
+WEIGHT_INIT: Weight initialization approach
 0 - uniform distribution
 1 - zeros
 2 - random between 0 and 1
 """
-WEIGHT_INIT = 0
