eda5c0f34c07da5ae63ebb7e69bfdb4111205a0f
Sergi Nogués Farrés <nogues.s.6@gmail.com>
2021-11-01 13:10:51 +0100

    backprop works and optimal hyperparams

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ class NN:
     Shallow neural network
     """
 
-    def __init__(self, input_dim=8, hidden_dim=3, output_dim=8, lr=0.01, w_d=0.001):
+    def __init__(self, input_dim=8, hidden_dim=3, output_dim=8, lr=0.1, w_d=0.001, epochs=346146):
         """
         :param x: learning samples, a 2D array of shape (8,8)
         :param y: desired output, a 2D array of shape (8,8)
@@ -37,23 +37,68 @@ class NN:
         self.output = np.zeros(self.y.shape)
         self.lr = lr
         self.weight_decay = w_d
-        # nn weights with 1 bias node per layer (except output layer)
-        self.synapse_0 = np.random.random((self.input_dim+1, self.hidden_dim))
-        self.synapse_1 = np.random.random((self.hidden_dim+1, self.output_dim))
+        self.epochs = epochs
+
+        self.cost = 10
+
+        self.synapse_0 = np.random.uniform(-.05, .05, (self.hidden_dim, self.input_dim + 1))
+        self.synapse_1 = np.random.uniform(-.05, .05, (self.output_dim, self.hidden_dim + 1))
+
+        self.gradients_0 = np.zeros(self.synapse_0.shape)
+        self.gradients_1 = np.zeros(self.synapse_1.shape)
 
     def train(self):
-        for _ in range(10000):
+        train = True
+        i = 0
+        while train:
+            i = i+1
+            print("epoch:", i)
             self.fit()
+            if self.test():
+                train = False
+        self.test(True)
 
-    def test(self):
+    def test(self, print_=False):
         n_correct = 0
-        n_samples = 8
+        CONVERGED = False
         for sample in self.input:
-            predicted_y, a_hidden_layer, z_hidden, z_output = self.forward(sample)
-            if (predicted_y == sample).all():
+            predicted_y, a_hidden_layer = self.forward(sample)
+            if np.argmax(sample) == np.argmax(predicted_y):
                 n_correct += 1
+            if print_:
+                print("predicted:", np.argmax(predicted_y))
+                print("label:", np.argmax(sample))
+        if n_correct == 8:
+            CONVERGED = True
 
-        print("n_correct = " + str(n_correct))
+        return CONVERGED
+
+    def fit(self):
+        # one gradient per weight
+        self.gradients_0 = np.zeros(self.synapse_0.shape)
+        self.gradients_1 = np.zeros(self.synapse_1.shape)
+        cost = 0
+
+        for sample in self.input:
+            # get rid of the bias parameter
+            desired_y = sample
+            # sample (i.e.) = [0,1,0,0,0,0,0,0]
+
+            # forward pass
+            predicted_y, a_hidden_layer = self.forward(sample)
+
+            # print the cost
+            cost = cost + 0.5 * (np.linalg.norm(predicted_y - desired_y) ** 2)
+
+            # compute the delta errors by doing a backward pass
+            delta_output_layer, delta_hidden_layer = self.compute_deltas(predicted_y, desired_y, a_hidden_layer)
+
+            # partial derivatives for each weight
+            self.update_gradients(delta_output_layer, a_hidden_layer, delta_hidden_layer, sample)
+
+        print(cost / self.input_dim)
+        self.cost = cost / self.input_dim
+        self.update_weights()
 
     def activation(self, input, layer):
         """
@@ -69,18 +114,18 @@ class NN:
             weights = self.synapse_1
 
         # sum + bias
-        z = np.dot(input, weights[1:]) + weights[0]
+        z = np.dot(weights[:, 1:], input) + weights[:, 0]
 
         # sigmoid transfer
         activation = self.sigmoid(z)
 
-        return activation, z
+        return activation
 
     def forward(self, x):
-        a_hidden_layer, z_hidden = self.activation(x, 1)
-        predicted_y, z_output = self.activation(a_hidden_layer, 2)
+        a_hidden_layer = self.activation(x, 1)
+        predicted_y = self.activation(a_hidden_layer, 2)
 
-        return predicted_y, a_hidden_layer, z_hidden, z_output
+        return predicted_y, a_hidden_layer
 
     def compute_deltas(self, predicted_y, desired_y, a_hidden_layer):
         """
@@ -90,76 +135,42 @@ class NN:
         :param a_hidden_layer: activation values for each of the 3 nodes in hidden layer
         """
         # compute the error delta(3) = delta_output_layer
-        delta_output_layer = -1 * self.sigmoid_derivative(predicted_y) * (desired_y - predicted_y)
+        delta_output_layer = -1 * (desired_y - predicted_y) * self.sigmoid_derivative(predicted_y)
 
         # compute delta(2)
-        delta_hidden_layer = np.dot(delta_output_layer, self.synapse_1.T[:, 1:]) * self.sigmoid_derivative(a_hidden_layer)
+        delta_hidden_layer = np.dot(self.synapse_1.T[1:], delta_output_layer) * self.sigmoid_derivative(a_hidden_layer)
 
         return delta_output_layer, delta_hidden_layer
 
-    def weight_derivative(self, delta, activation):
-        """ Compute desired partial derivatives """
-        d_weights = np.zeros((len(activation), len(delta)))
-        for j in range(len(activation)):
-            for i in range(len(delta)):
-                d_weights[j, i] += activation[j] * delta[i]
+    def update_gradients(self, delta_output_layer, a_hidden_layer, delta_hidden_layer, sample):
+        """ Update the partial derivatives """
 
-        return d_weights
+        # synapse 1
+        for i in range(len(delta_output_layer)):
+            self.gradients_1[i, 0] = self.gradients_1[i, 0] + delta_output_layer[i]
+            for j in range(len(a_hidden_layer)):
+                self.gradients_1[i, j + 1] = self.gradients_1[i, j + 1] + a_hidden_layer[j] * delta_output_layer[i]
 
-    def bias_derivative(self, delta):
-        d_bias = np.zeros((1, len(delta)))
-        for i in range(len(delta)):
-            d_bias[0, i] += delta[i]
+        # synapse 0
+        for i in range(len(delta_hidden_layer)):
+            self.gradients_0[i, 0] = self.gradients_0[i, 0] + delta_hidden_layer[i]
+            for j in range(len(sample)):
+                self.gradients_0[i, j + 1] = self.gradients_0[i, j + 1] + sample[j] * delta_hidden_layer[i]
 
-        return d_bias
-
-    def update_weights(self, d_weights_1, d_bias_1, d_bias_0, d_weights_0):
+    def update_weights(self):
         # compute cost gradient dJ/dtheta
         cost_gradient_synapse1 = np.zeros(self.synapse_1.shape)
         cost_gradient_synapse0 = np.zeros(self.synapse_0.shape)
-        m_1 = 1 / self.input_dim
 
-        cost_gradient_synapse1[1:, :] = (m_1 * d_weights_1) + (self.weight_decay * self.synapse_1[1:, :])
-        cost_gradient_synapse0[1:, :] = (m_1 * d_weights_0) + (self.weight_decay * self.synapse_0[1:, :])
+        cost_gradient_synapse1[:, 1:] = (self.gradients_1[:, 1:] + (self.weight_decay * self.synapse_1[:, 1:])) / self.input_dim
+        cost_gradient_synapse0[:, 1:] = (self.gradients_0[:, 1:] + (self.weight_decay * self.synapse_0[:, 1:])) / self.input_dim
 
-        cost_gradient_synapse1[0, :] = m_1 * d_bias_1
-        cost_gradient_synapse0[0, :] = m_1 * d_bias_0
+        cost_gradient_synapse1[:, 0] = self.gradients_1[:, 0] / self.input_dim
+        cost_gradient_synapse0[:, 0] = self.gradients_0[:, 0] / self.input_dim
 
         # update weights with gradient descent
-        self.synapse_0 -= self.lr * cost_gradient_synapse0
-        self.synapse_1 -= self.lr * cost_gradient_synapse1
-
-    def fit(self):
-        # one gradient per weight
-        d_weights_0 = np.zeros((self.input_dim, self.hidden_dim))
-        d_bias_0 = np.zeros((1, self.hidden_dim))
-        d_weights_1 = np.zeros((self.hidden_dim, self.output_dim))
-        d_bias_1 = np.zeros((1, self.output_dim))
-        average_cost = 0
-
-        for sample in self.input:
-            # get rid of the bias parameter
-            desired_y = sample
-            # sample (i.e.) = [0,1,0,0,0,0,0,0]
-
-            # forward pass
-            predicted_y, a_hidden_layer, z_hidden, z_output = self.forward(sample)
-
-            # print the cost
-            cost = 0.5 * (np.abs(predicted_y-desired_y)**2)
-            average_cost += np.mean(cost)
-
-            # compute the delta errors by doing a backward pass
-            delta_output_layer, delta_hidden_layer = self.compute_deltas(predicted_y, desired_y, a_hidden_layer)
-
-            # partial derivatives for each weight
-            d_weights_1 += self.weight_derivative(delta_output_layer, a_hidden_layer)
-            d_bias_1 += self.bias_derivative(delta_output_layer)
-            d_weights_0 += self.weight_derivative(delta_hidden_layer, sample)
-            d_bias_0 += self.bias_derivative(delta_hidden_layer)
-
-        print(average_cost/self.input_dim)
-        self.update_weights(d_weights_1, d_bias_1, d_bias_0, d_weights_0)
+        self.synapse_0 = self.synapse_0 - self.lr * cost_gradient_synapse0
+        self.synapse_1 = self.synapse_1 - self.lr * cost_gradient_synapse1
 
     @staticmethod
     def sigmoid_derivative(a):
